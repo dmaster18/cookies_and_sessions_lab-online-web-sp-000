08beab0d45fe610391b1c1fee3242e5b52c555bd
David Strauch <dmaster18@gmail.com>
2021-01-24 05:12:04 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
+  
 Rails.application.routes.draw do
-  resources :products,  only: [:index, :add]
-  post '/application/homepage', to: "application#homepage"
-  get '/', to: "application#homepage"
-  post '/', to: "application#homepage"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
+  root 'products#index'
+
+  post '/' => 'products#add'
+
 end
